7b7f3da8827583cfbb86e989d06f1c4677a86859
Berk Certel <m.berkcertel@outlook.com>
2025-06-21 22:54:00 +0300

    prisma adapter added, jwt and session added,  sign Up added

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,25 +2,69 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import GitHub from "next-auth/providers/github";
+import db from "./db/db";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { schema } from "./schema";
+import { v4 as uuid } from "uuid";
+import { encode } from "next-auth/jwt";
+
+const adapter = PrismaAdapter(db);
 
 export const { auth, handlers, signIn } = NextAuth({
+  adapter,
   providers: [
     GitHub,
     Credentials({
       credentials: { email: {}, password: {} },
       authorize: async (credentials) => {
-        const email = "admin@admin.com";
-        const password = "1234";
-        if (credentials.email === email && credentials.password === password) {
-          return {
-            id: "1",
-            name: "Admin",
-            email,
-          };
-        } else {
+        const validatedCredentials = schema.parse(credentials);
+
+        const user = await db.user.findFirst({
+          where: {
+            email: validatedCredentials?.email,
+            password: validatedCredentials?.password,
+          },
+        });
+
+        if (!user) {
           throw new Error("Invalid credentials");
         }
+
+        return user;
       },
     }),
   ],
+
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account?.provider === "credentials") {
+        token.credentials = true;
+      }
+      return token;
+    },
+  },
+
+  jwt: {
+    encode: async function (params: any) {
+      if (params.token?.credentials) {
+        const sessionToken = uuid();
+        if (!params.token?.sub) {
+          throw new Error("Credentials are not supported for JWT encoding");
+        }
+
+        const createdSession = await adapter?.createSession?.({
+          sessionToken: sessionToken,
+          userId: params.token.sub,
+          expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days
+        });
+
+        if (!createdSession) {
+          throw new Error("Failed to create session");
+        }
+
+        return sessionToken;
+      }
+      return encode(params);
+    },
+  },
 });
